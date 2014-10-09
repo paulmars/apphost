cb28f5ed2cb2dbabc94167a69acc6c1fa8ffbf8a
Paul McKellar <paul.mckellar@gmail.com>
2014-10-08 18:13:57 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users #, :path => '/',
-  #   path_names: {
-  #     sign_in: 'login',
-  #     sign_out: 'logout',
-  #     password: 'password',
-  #     confirmation: 'verification',
-  #     unlock: 'unblock',
-  #     registration: 'signup',
-  #     sign_up: 'new'
-  #   }
-  # devise_scope :admin do
-  #   get '/logout' => 'devise/sessions#destroy'
-  # end
+  devise_for :users, :path => '/',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'password',
+      confirmation: 'verification',
+      unlock: 'unblock',
+      registration: 'signup',
+      sign_up: 'new'
+    }
+  devise_scope :users do
+    get '/logout' => 'devise/sessions#destroy'
+  end
 
 end
