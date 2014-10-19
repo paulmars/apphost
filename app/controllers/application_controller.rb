7212d4349f46a85ae6ee2f0f8059ff2930a65dbd
Paul McKellar <paul.mckellar@gmail.com>
2014-10-19 14:30:33 -0700

    check for ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :secure_prod
+
+private
+
+  def secure_prod
+    return unless Rails.env.production?
+    return unless request.ssl?
+    redirect_to "https://apphoster.herokuapp.com/"
+  end
+
 end
